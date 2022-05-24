1a4b9431d31ee40012940632a978d8056cec5f20
abrikosechek <abrikosechek@gmail.com>
2022-05-24 05:48:15 +0400

    a

---
+++ client/src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="home">
+    <h1>Home page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
